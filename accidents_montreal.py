4479c5e0e07c548b7831d09849b7c01b37862277
Antoine Hébert <hantoine02@gmail.com>
2019-03-08 09:46:46 -0500

    Add download of the documentation for montreal accidents dataset

--- accidents_montreal.py
+++ accidents_montreal.py
@@ -7,15 +7,17 @@ import dask.dataframe as dd
 import pandas as pd
 
 def fetch_accidents_montreal():
-    if isfile('data/accidents-montreals.zip.lock'):
+    if isfile('data/accidents-montreal.lock'):
         print('Skip fetching montreal accidents dataset: already downloaded')
         return
     url = 'http://donnees.ville.montreal.qc.ca/dataset/cd722e22-376b-4b89-9bc2-7c7ab317ef6b/resource/05deae93-d9fc-4acb-9779-e0942b5e962f/download/accidents_2012_2017.zip'
+    url_variable_desc = 'https://saaq.gouv.qc.ca/donnees-ouvertes/rapports-accident/rapports-accident-documentation.pdf'
     print('Fetching montreal accidents dataset...')
     try:
         urlretrieve(url, 'data/accidents-montreal.zip')
+        urlretrieve(url_variable_desc, 'data/accident-montreal-documentation.pdf')
         print('Fetching montreal accidents dataset: done')
-        open('data/accidents-montreals.zip.lock', 'w').close()
+        open('data/accidents-montreal.lock', 'w').close()
     except (URLError, HTTPError):
         print('Unable to reach montreal accidents dataset server')
 
