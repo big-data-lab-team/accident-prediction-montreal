5cc22ef0d4fd5f7cc523549a650d2936536c4bc0
GTimothee <ava_punksmash@protonmail.com>
2019-03-14 20:32:23 -0400

    added working notebook + comments + changed urllib to requests

--- weather.py
+++ weather.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from os import mkdir
 from os.path import isdir, isfile
-
+from requests import get
 
 def get_weather(lat, long, year, month, day, hour):
+    ''' Get the weather at a given location at a given time.
+    '''
     stations = get_stations(lat, long, year, month, day)
     weighted_average_num = 0
     weighted_average_denum = 0
@@ -17,6 +19,8 @@ def get_weather(lat, long, year, month, day, hour):
 
 
 def get_stations(lat, long, year, month, day):
+    ''' Get data from all stations.
+    '''
     lat = degree_to_DMS(lat)
     long = degree_to_DMS(long)
     url = (f'http://climate.weather.gc.ca/historical_data/'
@@ -27,7 +31,7 @@ def get_stations(lat, long, year, month, day):
            f'txtCentralLongMin={long[1]}&txtCentralLongSec={long[2]:.1f}&'
            f'StartYear=1840&EndYear=2019&optLimit=specDate&Year={year}&'
            f'Month={month}&Day={day}&selRowPerPage=100')
-    page = BeautifulSoup(urlopen(url), 'lxml')
+    page = BeautifulSoup(get(url).content, 'lxml')
     stations = (page.body.main
                 .find('div',
                       class_='historical-data-results proximity hidden-lg')
@@ -36,6 +40,8 @@ def get_stations(lat, long, year, month, day):
 
 
 def get_station_temp(id, year, month, day, hour):
+    ''' Get temperature for a given station.
+    '''
     cache_file_path = f'data/weather/s{id}_{year}_{month}.h5'
     if isfile(cache_file_path):
         return (pd.read_hdf(cache_file_path, key='w')
@@ -54,6 +60,8 @@ def get_station_temp(id, year, month, day, hour):
 
 
 def parse_station(s):
+    ''' Utility function for get_stations.
+    '''
     return (
         int(s.find('input', {'name': 'StationID'})['value']),
         float(s.find_all('div', class_='row',
@@ -62,11 +70,19 @@ def parse_station(s):
 
 
 def degree_to_DMS(degree):
+    ''' Convert from plain degrees format to DMS format of geolocalization.
+    DMS: "Degrees, Minutes, Seconds" is a format for coordinates at the surface
+        of earth. Decimal Degrees = degrees + (minutes/60) + (seconds/3600)
+        This measure permit to gain in precision when a degree is not precise
+        enough.
+    '''
     return (int(degree), int(60 * (abs(degree) % 1)),
             ((60 * (abs(degree) % 1)) % 1) * 60)
 
 
 def skip_header(file):
+    ''' Utility function for get_station_temp.
+    '''
     n_emptyLineMet = 0
     while n_emptyLineMet < 2:
         if file.readline() == b'\n':
