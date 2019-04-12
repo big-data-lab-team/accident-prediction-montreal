4611086d8af679bf58767c1092f08aa51048051d
GTimothee <ava_punksmash@protonmail.com>
2019-04-12 12:36:47 -0700

    Clean pyspark_submit.sh script to launch job on cluster

--- pyspark_submit.sh
+++ pyspark_submit.sh
@@ -6,42 +6,54 @@
 #SBATCH --mail-type=REQUEUE
 #SBATCH --mail-type=ALL
 #SBATCH --account=def-glatard
-#SBATCH --time=02:00:00
-#SBATCH --nodes=4
-#SBATCH --mem=4G
-#SBATCH --cpus-per-task=8
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=40G
+#SBATCH --cpus-per-task=32
 #SBATCH --ntasks-per-node=1
-module load spark/2.3.0
 
-# Recommended settings for calling Intel MKL routines from multi-threaded applications
-# https://software.intel.com/en-us/articles/recommended-settings-for-calling-intel-mkl-routines-from-multi-threaded-applications 
+module load spark/2.3.0
+module load python/3.7
+
+source ~/acc_env/bin/activate
+
+export PYSPARK_PYTHON="/home/tguedon/acc_env/bin/python"
+export PYTHONPATH=${PYTHONPATH}:${PWD}
 export MKL_NUM_THREADS=1
 export SPARK_IDENT_STRING=$SLURM_JOBID
 export SPARK_WORKER_DIR=$SLURM_TMPDIR
 export SLURM_SPARK_MEM=$(printf "%.0f" $((${SLURM_MEM_PER_NODE} *95/100)))
+
+#start master
 start-master.sh
-sleep 5
-MASTER_URL=$(grep -Po '(?=spark://).*' $SPARK_LOG_DIR/spark-${SPARK_IDENT_STRING}-org.apache.spark.deploy.master*.out)
+sleep 20
+
+MASTER_URL_STRING=$(grep -Po '(?=spark://).*' $SPARK_LOG_DIR/spark-${SPARK_IDENT_STRING}-org.apache.spark.deploy.master*.out)
+
+IFS=' '
+read -ra MASTER_URL <<< "$MASTER_URL_STRING"
+echo "master url :" ${MASTER_URL}
+
 NWORKERS=$((SLURM_NTASKS - 1))
+
+echo "----->" ${NWORKERS}
+echo "----->" ${SPARK_LOG_DIR}
+echo "----->" ${SLURM_CPUS_PER_TASK}
+echo "----->" ${MASTER_URL}
+echo "----->" ${SLURM_SPARK_MEM}
+
 SPARK_NO_DAEMONIZE=1 srun -n ${NWORKERS} -N ${NWORKERS} --label --output=$SPARK_LOG_DIR/spark-%j-workers.out start-slave.sh -m ${SLURM_SPARK_MEM}M -c ${SLURM_CPUS_PER_TASK} ${MASTER_URL} &
 slaves_pid=$!
-
-module load python/3.7
-#virtualenv --no-download $SLURM_TMPDIR/env
-#source $SLURM_TMPDIR/env/bin/activate
-#pip install --upgrade pip
-#pip install --no-index -r requirements.txt
-
-export PYSPARK_PYTHON="/home/tguedon/acc_env/bin/python"
-export PYTHONPATH=${PYTHONPATH}:${PWD}
 
 acc=/home/tguedon/projects/def-glatard/tguedon/accident-prediction-montreal/accidents_montreal.py
 road=/home/tguedon/projects/def-glatard/tguedon/accident-prediction-montreal/road_network.py
 weather=/home/tguedon/projects/def-glatard/tguedon/accident-prediction-montreal/weather.py
 preprocess=/home/tguedon/projects/def-glatard/tguedon/accident-prediction-montreal/preprocess.py
 utils=/home/tguedon/projects/def-glatard/tguedon/accident-prediction-montreal/utils.py
+random_forest=/home/tguedon/projects/def-glatard/tguedon/accident-prediction-montreal/random_forest.py
 
-srun -n 1 -N 1 spark-submit /home/tguedon/projects/def-glatard/tguedon/accident-prediction-montreal/main_sample_match.py --master ${MASTER_URL} --py-files ${acc} ${road} ${weather} ${preprocess} ${utils} --executor-memory ${SLURM_SPARK_MEM}M
+srun -n 1 -N 1 spark-submit --master ${MASTER_URL} --executor-memory ${SLURM_SPARK_MEM}M /home/tguedon/projects/def-glatard/tguedon/accident-prediction-montreal/main_negative_samples.py --py-files ${acc} ${road} ${weather} ${preprocess} ${utils} ${random_forest}
 
 kill $slaves_pid
 stop-master.sh
