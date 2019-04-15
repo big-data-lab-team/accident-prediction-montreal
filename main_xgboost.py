ad8ca2cf054a6b34477aac6e95751b5d1a4c6efb
Antoine Hébert <hantoine02@gmail.com>
2019-04-14 22:55:04 -0700

    Add workdir to main_xgboost.py

--- main_xgboost.py
+++ main_xgboost.py
@@ -8,6 +8,7 @@ from utils import init_spark
 from preprocess import get_positive_samples, \
                        get_negative_samples, \
                        get_dataset_df
+from workdir import workdir
 
 
 from pyspark.sql import SparkSession
@@ -24,7 +25,7 @@ from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
 
 def save_model():
     workdir = "./"
-    path = "data/xgboost.model"
+    path = workdir + "data/xgboost.model"
     if os.path.isdir(path):
         shutil.rmtree(path)
     model.save(path)
@@ -32,7 +33,7 @@ def save_model():
 
 
 def grid_search(spark):
-    sample_ratio = 0.1
+    sample_ratio = 0.05
     neg_samples = get_negative_samples(spark).sample(sample_ratio).na.fill(0)
     pos_samples = get_positive_samples(spark).sample(sample_ratio).na.fill(0)
     df = get_dataset_df(spark, pos_samples, neg_samples).na.fill(0)
@@ -72,6 +73,6 @@ def grid_search(spark):
 
 spark = init_spark()
 #os.environ['PYSPARK_SUBMIT_ARGS'] = '--jars data/xgboost4j-spark-0.72.jar,data/xgboost4j-0.72.jar pyspark-shell'
-spark.sparkContext.addPyFile("data/sparkxgb.zip")
+spark.sparkContext.addPyFile(workdir + "data/sparkxgb.zip")
 from sparkxgb import XGBoostEstimator
 grid_search(spark)
