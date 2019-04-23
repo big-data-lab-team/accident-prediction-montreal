bb1af51f0cfc192fec67a27411512d7d3802ee51
Antoine Hébert <hantoine02@gmail.com>
2019-04-23 11:16:26 -0400

    Add more evaluation metrics

--- evaluate.py
+++ evaluate.py
@@ -13,6 +13,20 @@ def evaluate_binary_classifier(predictions):
                                           labelCol='label',
                                           metricName='f1')
     f1_score = f1_evaluator.evaluate(predictions)
-    print(f"Area Under PR = {area_under_PR}\nF1 score = {f1_score}")
+    ROC_evaluator = \
+        BinaryClassificationEvaluator(labelCol="label",
+                                      rawPredictionCol="rawPrediction",
+                                      metricName="areaUnderROC")
+    area_under_ROC = ROC_evaluator.evaluate(predictions)
+    acc_evaluator = \
+    MulticlassClassificationEvaluator(predictionCol='prediction',
+                                      labelCol='label',
+                                      metricName='accuracy')
+    acc_score = acc_evaluator.evaluate(predictions)
 
-    return (area_under_PR, f1_score)
+    print(f"Area Under PR = {area_under_PR}")
+    print(f"F1 score = {f1_score}")
+    print(f"Area Under ROC = {area_under_ROC}")
+    print(f"Accuracy = {acc_score}")
+
+    return (area_under_PR, f1_score, area_under_ROC, acc_score)
