0490ad663d9780bec1d7dfdd89113e03851758a3
Antoine Hébert <hantoine02@gmail.com>
2019-04-26 19:02:14 -0400

    Extract exportation of results from main script to export_results.py

--- mains/main_train_urf.py
+++ mains/main_train_urf.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python
-from os import mkdir
-from os.path import isdir
 from preprocess import get_negative_samples, get_positive_samples
 from utils import init_spark
 from preprocess import get_dataset_df
 from pyspark.ml.classification import RandomForestClassifier
-from pyspark.ml.evaluation import BinaryClassificationEvaluator
-from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit, CrossValidator
 from pyspark.ml import Pipeline
 from random_undersampler import RandomUnderSampler
-from workdir import workdir
-from random_forest import compute_precision_recall_graph
 from random_forest import get_feature_importances
-from evaluate import evaluate_binary_classifier
+from export_results import *
 
-# Prepare result directory
-i = 1
-while isdir(workdir + f'data/urf_{i}'):
-    i += 1
-mkdir(workdir + f'data/urf_{i}')
-
+result_dir = create_result_dir('urf')
 spark = init_spark()
-neg_samples = get_negative_samples(spark).sample(1.0)
+neg_samples = get_negative_samples(spark).sample(0.5)
 pos_samples = get_positive_samples(spark)
 
 imbalance_ratio = (neg_samples.count()/pos_samples.count())
@@ -45,42 +34,12 @@ ru = (RandomUnderSampler()
       .setTargetImbalanceRatio(1.0))
 pipeline = Pipeline().setStages([ru, rf])
 model = pipeline.fit(train_set)
-
-
-# Write model hyper-parameters
-def write_params(model, path):
-  with open(path, 'w') as file:    
-    for stage in model.stages:
-          params = stage.extractParamMap()
-          for k in params:
-              file.write(f'{k.name}: {params[k]}\n')
-
-write_params(model, workdir + f'data/urf_{i}/params')
-
-
 predictions = model.transform(test_set).persist()
 train_predictions = model.transform(train_set).persist()
 
-# Write results
-with open(workdir + f'data/urf_{i}/results', 'w') as file:
-    test_res = evaluate_binary_classifier(predictions)
-    train_res = evaluate_binary_classifier(train_predictions)
-    file.write('Test set:\n')
-    file.write(f"\tArea Under PR = {test_res[0]}\n")
-    file.write(f"\tF1 score = {test_res[1]}\n")
-    file.write(f"\tArea Under ROC = {test_res[2]}\n")
-    file.write(f"\tAccuracy = {test_res[3]}\n")
-    file.write('Train set:\n')
-    file.write(f"\tArea Under PR = {train_res[0]}\n")
-    file.write(f"\tF1 score = {train_res[1]}\n")
-    file.write(f"\tArea Under ROC = {train_res[2]}\n")
-    file.write(f"\tAccuracy = {train_res[3]}\n")
+write_params(model, result_dir)
+write_results(predictions, train_predictions, result_dir)
 
 # Write feature importances
 feature_importances = get_feature_importances(model.stages[1])
-feature_importances.to_csv(workdir + f'data/urf_{i}/feature_importances.csv')
-
-# Write precision recall curve
-precision_recall = compute_precision_recall_graph(predictions, 20)
-precision_recall.to_csv(workdir + f'data/urf_{i}/precision_recall.csv')
-
+feature_importances.to_csv(result_dir + '/feature_importances.csv')
