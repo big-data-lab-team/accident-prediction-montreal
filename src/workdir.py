0a94307a463e1ed809797d314ea0e2287271817b
Antoine Hebert <hantoine02@gmail.com>
2019-10-18 15:22:18 -0400

    Add default work directory config

---
+++ src/workdir.py
@@ -0,0 +1,7 @@
+"""
+    Configuration file used to specify the work directory used by all the scripts
+"""
+import os
+
+# directory in which the data directory will be created/used
+workdir = os.getcwd() + '/'
